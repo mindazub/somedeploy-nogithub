4c89a4813884bfcd5cbab8dfb60c7a0c17e78838
Mindaugas <mindaugas.azubalis@gmail.com>
2024-07-03 16:21:50 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import nice from "./nice.svg";
+import "./App.css";
 
 function App() {
+  const [joke, setJoke] = useState("");
+  const [seconds, setSeconds] = useState(33);
+
+  useEffect(() => {
+    const fetchJoke = async () => {
+      try {
+        const response = await fetch(
+          "https://official-joke-api.appspot.com/random_joke"
+        );
+        const data = await response.json();
+        setJoke(`${data.setup} - ${data.punchline}`);
+      } catch (error) {
+        console.error("Error fetching the joke:", error);
+      }
+    };
+
+    fetchJoke();
+    const jokeInterval = setInterval(() => {
+      fetchJoke();
+      setSeconds(60); // Reset the countdown timer
+    }, 60000);
+
+    return () => clearInterval(jokeInterval);
+  }, []);
+
+  useEffect(() => {
+    const countdownInterval = setInterval(() => {
+      setSeconds((prevSeconds) => (prevSeconds > 0 ? prevSeconds - 1 : 60));
+    }, 1000);
+
+    return () => clearInterval(countdownInterval);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={nice} className="nice" alt="nice" width={200} />
+        <p>{joke ? joke : "Loading joke..."}</p>
+        <p>Next joke in: {seconds}s</p>
       </header>
     </div>
   );
